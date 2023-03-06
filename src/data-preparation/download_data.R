eb558f50f412b108d370e10824c01d77e7249ab8
Hannes Datta <H.Datta@tilburguniversity.edu>
2023-03-06 14:33:43 +0100

    some dummy commit

--- src/data-preparation/download_data.R
+++ src/data-preparation/download_data.R
@@ -6,3 +6,4 @@ download.file('https://rgreminger.github.io/files/dataset1.csv','./data/dataset1
 # dir.create('./data/dataset2')  # Uncomment if need to create directory with R
 download.file('https://rgreminger.github.io/files/dataset2.csv','./data/dataset2/dataset2.csv')
 
+# 
