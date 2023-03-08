caa02ff1bc9f1e4442632f7a39e882dc488936c5
Lisa Holling <l.holling@tilburguniversity.edu>
2023-03-08 16:01:26 +0100

    I created two dataframes calles listings_short and listings_long that contain unique listing_ids based on the consecutive count of availability

---
+++ src/data-preparation/20-transform.R
@@ -0,0 +1,33 @@
+library(tidyverse)
+library(dplyr)
+
+calendar <- read_csv("calendar.csv.gz")
+
+# New column 0-1
+calendar <- calendar %>% mutate(available1 = (as.numeric(!available)))
+
+# Create a function to count consecutive ranges of 1
+
+## Create a new column 'consecutive_count' and initialize it to zero
+calendar$consecutive_count <- 0
+
+## Find the start and end indices of consecutive ranges of 1's
+start_indices <- which(diff(c(0, calendar$available1)) == 1)
+end_indices <- which(diff(c(calendar$available1, 0)) == -1)
+
+## Loop through each consecutive range and assign its length to the 'consecutive_count' column
+for (i in seq_along(start_indices)) {
+  calendar$consecutive_count[start_indices[i]:end_indices[i]] <- end_indices[i] - start_indices[i] + 1
+}
+
+
+# New dataset with only listing_id's > 28
+listings_long <- calendar %>% group_by(listing_id) %>% filter(consecutive_count >= 28)
+listings_short <- calendar %>% anti_join(listings_long, by = "listing_id") %>% group_by(listing_id) %>% filter(consecutive_count < 28) 
+
+# Only return unique listing_id's
+listings_long <- unique(listings_long$listing_id)
+listings_short <- unique(listings_short$listing_id)
+
+sum(length(listings_long), length(listings_short))
+length(unique(calendar$listing_id))
