bffc15320f0a3f54560ec04af6926801592e3990
Lisa Holling <l.holling@tilburguniversity.edu>
2023-03-12 15:55:29 +0100

    Changes in transform_data: downloaded csv files of listing_type and listings_sorted

--- src/data-preparation/transform_data.R
+++ src/data-preparation/transform_data.R
@@ -1,28 +1,27 @@
 library(tidyverse)
 library(dplyr)
 
-calendar <- read_csv("calendar.csv")
-
-# New column 0-1
+# Calendar dataset: creating listings
+## New column 0-1
 calendar <- calendar %>% mutate(available1 = (as.numeric(!available)))
 
-# Create a function to count consecutive ranges of 1
+## Create a function to count consecutive ranges of 1
 
-## Create a new column 'consecutive_count' and initialize it to zero
+### Create a new column 'consecutive_count' and initialize it to zero
 calendar$consecutive_count <- 0
 
-## Find the start and end indices of consecutive ranges of 1's
+### Find the start and end indices of consecutive ranges of 1's
 start_indices <- which(diff(c(0, calendar$available1)) == 1)
 end_indices <- which(diff(c(calendar$available1, 0)) == -1)
 
-## Loop through each consecutive range and assign its length to the 'consecutive_count' column
+### Loop through each consecutive range and assign its length to the 'consecutive_count' column
 for (i in seq_along(start_indices)) {
   calendar$consecutive_count[start_indices[i]:end_indices[i]] <- end_indices[i] - start_indices[i] + 1
 }
 
-# Create new dataframes
 ## New dataframe with only listing_id's > 28
 listings_long <- calendar %>% group_by(listing_id) %>% filter(consecutive_count >= 28)
+
 ## New dataframe with only listing_id's < 28 
 listings_short <- calendar %>% anti_join(listings_long, by = "listing_id") %>% group_by(listing_id) %>% filter(consecutive_count < 28) 
 
@@ -34,17 +33,17 @@ listings_short <- unique(listings_short$listing_id)
 sum(length(listings_long), length(listings_short)) #6898
 length(unique(calendar$listing_id)) #6898
 
-# Format the new lists
+## Format the new lists
 
-## Create a tibble with the listings_long and listings_short
-listings <- tibble(listing_id = c(listings_long, listings_short),
+### Create a tibble with the listings_long and listings_short
+listing_type <- tibble(listing_id = c(listings_long, listings_short),
                    listing_type = c(rep("long", length(listings_long)),
                                     rep("short", length(listings_short))))
 
-## importing listings dataset 
-listings_unsorted <- read_csv("listings-2.csv")
-
-#deleting unimportant columns 
+## creating a subset of important columns 
 listings_sorted <- subset(listings_unsorted, select = c(id, name, neighbourhood, property_type, room_type, accommodates, bathrooms_text, bedrooms, beds, amenities, price, minimum_nights, maximum_nights))
 
+# Download created csv files
+write.csv(listing_type, "listing_type.csv", row.names = FALSE)
+write.csv(listings_sorted, "listings_sorted.csv", row.names = FALSE)
 
