c8d78a709a274271e6e82d1678e57d155a10934c
Lisa Holling <l.holling@tilburguniversity.edu>
2023-03-14 14:27:21 +0100

    we removed NAs, cleaned the data, added columns, etc.

--- src/data-preparation/clean_data.R
+++ src/data-preparation/clean_data.R
@@ -1,11 +1,42 @@
 # Load merged data 
-load("./gen/data-preparation/temp/data_merged.RData")
+#load("./gen/data-preparation/src/data-preparation/listings_joined.csv")
 
-# Drop observations with V1 <= -0.9
-df_cleaned <- df_merged[df_merged$V1 > -0.9,]
+# Missing values
+## Beds: remove 274 NA's
+listings_joined <- listings_joined %>% filter(!is.na(listings_joined$beds))
 
-# Remove V1
-df_cleaned <- df_cleaned[,c(1,2,4:7)]
+## Bedrooms: NA's -> 0 (studio)
+listings_joined <- listings_joined %>% 
+  mutate(bedrooms = ifelse(is.na(bedrooms), 0, bedrooms))
+
+# Bathrooms: remove 9 NA's
+listings_joined <- listings_joined %>% filter(!is.na(listings_joined$bathrooms_text))
+  
+# Check NA's for characters
+  
+  
+# Mutate columns
+## Bathrooms
+### private / shared
+listings_joined <- listings_joined %>% 
+  mutate(bathroom_shared = ifelse(grepl("shared", bathrooms_text, ignore.case = TRUE), "shared", "private"))
+
+## room_type
+### private / shared
+listings_joined <- listings_joined %>% 
+  mutate(room_shared = ifelse(grepl("shared", room_type, ignore.case = TRUE), "shared", "private"))
+
+View(listings_joined)
+
+## Character -> numeric
+### Bathrooms
+glimpse(listings_joined$bathrooms_text)
+unique(listings_joined$bathrooms_text)
+unique(listings_joined$room_type)
+
+## Amenities
+
+
 
 # Save cleaned data
 save(df_cleaned,file="./gen/data-preparation/output/data_cleaned.RData")
